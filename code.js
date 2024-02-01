c5d4f295828379c5939af92b9618053380dc6984
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-02-01 12:39:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length === 0) {
+        return 0;
+    }
+    const n = a.length;
+    const partSize = Math.floor(n / 3);
+    const sum1 = divideAndConquerSum(a.slice(0, partSize));
+    const sum2 = divideAndConquerSum(a.slice(partSize, 2 * partSize));
+    const sum3 = divideAndConquerSum(a.slice(2 * partSize, n));
+    return sum1 + sum2 + sum3;
 }
+
+
